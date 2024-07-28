321d06245fd7567327b7a6d8868ade5d82069a91
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2024-07-28 16:02:25 -0300

    Add form to create new book

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-const myLibrary = [new Book("The Hobbit", "J. R. R Tolkien", true), new Book("The Fellowship of the Ring", "J. R. R Tolkien", false)];
+const myLibrary = [
+  new Book("The Hobbit", "J. R. R Tolkien", true),
+  new Book("The Fellowship of the Ring", "J. R. R Tolkien", false),
+];
 
 function Book(title, author, read) {
-  this.title = title,
-  this.author = author,
-  this.read = read;
+  (this.title = title), (this.author = author), (this.read = read);
 }
 
-function addBookToLibrary() {}
+function createBookFromInput() {
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let read = document.querySelector("#read").checked;
+
+  let newBook = new Book(title, author, read);
+
+  return newBook;
+}
 
 function makePrintableBook(book) {
   let printableBook = document.createElement("ul");
-  
+
   let bookTitle = document.createElement("li");
   bookTitle.innerText = book.title;
 
@@ -20,12 +29,26 @@ function makePrintableBook(book) {
   let bookRead = document.createElement("li");
   bookRead.innerText = book.read === true ? "Already read" : "Pending";
 
-  printableBook.append(bookTitle, bookAuthor, bookRead)
-  return printableBook
+  printableBook.append(bookTitle, bookAuthor, bookRead);
+  return printableBook;
 }
 
-const bookShelf = document.querySelector("#bookshelf")
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
 
-myLibrary.forEach(book => {
-  bookShelf.appendChild(makePrintableBook(book))
-})
+const bookShelf = document.querySelector("#bookshelf");
+
+myLibrary.forEach((book) => {
+  bookShelf.appendChild(makePrintableBook(book));
+});
+
+const newBookButton = document.querySelector("#new-book");
+
+newBookButton.addEventListener("click", (event) => {
+  newBook = createBookFromInput();
+  addBookToLibrary(newBook);
+  bookShelf.appendChild(makePrintableBook(newBook));
+  event.preventDefault();
+});
+
